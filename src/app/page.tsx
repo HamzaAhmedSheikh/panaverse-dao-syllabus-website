51d16b64fad3af976ca09f5e3a88c28515ca6796
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2023-01-29 04:08:37 +0500

    For the Panavese DAO Website, I added some SEO meta tags and a footer.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Header from '../components/Header';
 import Intro from '@/components/Intro';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
     <>
      <Header />     
      <Intro /> 
+     <Footer />
     </>
   )
 }
